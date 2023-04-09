fc0d3fc3f9b951af34a4783e068aa88192888371
yarmail <yarmail@yandex.ru>
2023-04-09 16:00:33 +0300

    добавляем entity (модели, сущности)

---
+++ entity/src/main/java/com/example/entity/model/Task.java
@@ -0,0 +1,60 @@
+package com.example.entity.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "task")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "completed")
+    private Integer completed;
+
+    @Column(name = "task_date")
+    private Timestamp taskDate;
+
+    @ManyToOne
+    @JoinColumn(name = "priority_id", referencedColumnName = "id")
+    private Priority priority;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private Category category;
+}
+/*
+Примечания
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode.Include
+Эквалс и хешкод делаем по id, т.к. этого достаточно
+и быстро
+
+Из генерации удалил поля priorityId, categoryId,
+нужны будут объекты а не id.
+
+@ManyToOne
+Много задач имеют один приоритет и одну категорию
+
+@JoinColumn(name = "priority_id", referencedColumnName = "id")
+Благодаря этой связке при добавлении новых составных объектов
+Hibernate возьмет поле id у Priority и в таблице Task
+сохранит сохранит его в поле priority_id.
+ */
